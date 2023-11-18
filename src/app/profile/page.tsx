cb2f73c01cb8c11fec2e1d49a8109f0f517227b3
Fida ur Rahman <fidaurr900@gmail.com>
2023-11-18 15:18:20 +0500

    logout also completed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,29 @@
-
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import {toast} from "react-hot-toast";
 
 export default function ProfilePage() {
+    const router = useRouter();
+    const Logout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            toast.success("Logout Success");
+            router.push("/login");
+            
+        } catch (error: any) {
+            console.log(error);
+            toast.error(error.message);   
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
             <h1>Profile</h1>
             <h3>profile Page</h3>
+            <button onClick={Logout} className="p-2 bg-orange-500 rounded m-4">
+                logout
+            </button>
         </div>
     )
 }
